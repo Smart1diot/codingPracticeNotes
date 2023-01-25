8bc135782bd6d4aa3e97fe7fc404ec55d8cfff5f
Smart1diot <119809043+Smart1diot@users.noreply.github.com>
2023-01-25 09:59:27 -0800

    Update and rename Main1.cpp to objectUseInFunctionRef_FuncTemplateForTypes.cpp

--- objectUseInFunctionRef_FuncTemplateForTypes.cpp
+++ objectUseInFunctionRef_FuncTemplateForTypes.cpp
@@ -9,16 +9,16 @@ struct Triad
 	T third{};
 };
 
-template <typename T>
-void print(Triad<T> p)
+template <typename T> //template 
+void print(Triad<T>& p) //Function call that uses object reference as a parameter
 {
 	std::cout << '[' << p.first << ", " << p.second << ", " << p.third << ']' << '\n';
 }
 
 int main()
 {
-	Triad t1{ 1, 2, 3};
-	Triad t2{ 1.2, 3.4, 5.6 };
+	Triad t1{ 1, 2, 3};		//object with all same type
+	Triad t2{ 1.2, 3.4, 5.6 };	//object with all same type
 
 	print(t1);
 	print(t2);
