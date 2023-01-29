9083553ac953f46c149e6e6eeed4ce98a5f6317f
Smart1diot <119809043+Smart1diot@users.noreply.github.com>
2023-01-29 08:35:21 -0800

    Add files via upload

---
+++ Main1.cpp
@@ -0,0 +1,30 @@
+//Example using an array member selecter to change bool mid loop causing it to end the loop
+
+#include  <iostream>
+
+struct Rectangle
+{
+	int length{};
+	int width{};
+	double sex{};
+	bool swap{};
+};
+
+Rectangle rec[4]{};
+
+int main()
+{
+	
+	rec[0].length = 1;
+	rec[1].width = 2;
+	rec[2].sex = 3.6;
+	rec[3].swap = true; //Declaring true to bool array member for rectangle object "rec[4]"
+
+	while (true)
+	{
+		std::cout << "print then end" << '\n';
+		rec[3].swap = false; //Using rec[3].swap from object rec to stop loop from continuing 
+	}
+
+	return 0;
+}
