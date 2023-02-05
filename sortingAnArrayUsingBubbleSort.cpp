6a24f36fc223ce362708419a8842fff572073992
Smart1diot <119809043+Smart1diot@users.noreply.github.com>
2023-02-05 08:32:20 -0800

    Update sortingAnArrayUsingBubbleSort.cpp

--- sortingAnArrayUsingBubbleSort.cpp
+++ sortingAnArrayUsingBubbleSort.cpp
@@ -3,23 +3,26 @@
 
 int main()
 {
-	int array[]{6,3,2,9,7,1,5,4,8};
-	constexpr int length{sizeof(array) / sizeof(array[0])};
+	int array[]{6,3,2,9,7,1,5,4,8};								//create an int array with 9 members 
+	constexpr int length{sizeof(array) / sizeof(array[0])};					//create a constecpr int length variable with num of all members
 
-	for (int iteration{ 0 }; iteration < length - 1; ++iteration) 
+	for (int iteration{ 0 }; iteration < length - 1; ++iteration)				//loop that iterates through every member of the array till length-1
 	{
-		for (int currentIndex{ 0 }; currentIndex < length - 1; ++currentIndex) 
+		for (int currentIndex{ 0 }; currentIndex < length - 1; ++currentIndex)		//inner loop that deals with the current index in array
 		{
-			if (array[currentIndex] > array[currentIndex + 1])
+			if (array[currentIndex] > array[currentIndex + 1])			//if currentIndex is larger than currentIndex+1
 			{
-				std::swap(array[currentIndex], array[currentIndex + 1]);
+				std::swap(array[currentIndex], array[currentIndex + 1]);	//we swap the values of the currentIndex with the currentIndex+1
 			}
 		}
 	}
 
-	for (int z{ 0 }; z < length; ++z)
+												//At this point all of our members in the array have been sorted
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	for (int z{ 0 }; z < length; ++z)							//loop that iterates through all array members
 	{
-		std::cout << array[z] << ' ';
+		std::cout << array[z] << ' ';							//prints the current array index value
 	}
 
 
