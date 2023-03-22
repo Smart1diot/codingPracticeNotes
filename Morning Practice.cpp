179d6f3802a40b4411b31b4f0e1aee27927926d3
Smart1diot <119809043+Smart1diot@users.noreply.github.com>
2023-03-22 07:53:52 -0700

    Add files via upload

---
+++ Morning Practice.cpp
@@ -0,0 +1,20 @@
+// 3/21/2023 Recursion
+#include <iostream>
+//return the sum of all the integers between 1 (inclusive) and sumto (inclusive)
+//returns 0 for negative numbers
+int sumTo(int sumto)
+{
+	if (sumto <= 0)
+		return 0; // base case (termination condition when user passed in an unexpected arugument (0 or negative)
+	
+	if (sumto == 1)
+		return 1; // normal base case (termination condition)
+
+	return sumTo(sumto - 1) + sumto; // recursive function call
+}
+int main()
+{
+	sumTo(5);
+
+	return 0;
+}
