2b6c4bc2d80d15f3b91b8a88b858ba984fa3be02
Smart1diot <119809043+Smart1diot@users.noreply.github.com>
2023-05-02 10:28:50 -0700

    Add files via upload

---
+++ Object-oriented Programming/The hidden "this" Pointer/Morning Practice.cpp
@@ -0,0 +1,25 @@
+// using hidden "this" pointer to chain multiple class functions using the class itself
+#include <iostream>
+
+class Calc
+{
+private:
+	int m_value{};
+
+public:
+	Calc& add(int value) { m_value += value; return *this; }
+	Calc& sub(int value) { m_value -= value; return *this; }
+	Calc& mult(int value) { m_value *= value; return *this; }
+
+	int getValue() { return m_value; }
+};
+
+int main()
+{
+	Calc calc{};
+	calc.add(5).sub(3).mult(4);
+
+	std::cout << calc.getValue() << '\n';
+
+	return 0;
+}
