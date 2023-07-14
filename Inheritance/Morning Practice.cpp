5fd471db606e17184a6f15fc5c8ba08551d4a269
Smart1diot <119809043+Smart1diot@users.noreply.github.com>
2023-07-13 17:42:11 -0700

    Add files via upload

---
+++ Inheritance/Morning Practice.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <iostream>
+
+class Person
+{
+private:
+	std::string m_name;
+	int m_age;
+
+public:
+	Person(std::string_view name, int age)
+		: m_name{ name }, m_age{ age }
+	{}
+
+	const std::string& getName() const { return m_name; }
+	int getAge() const { return m_age; }
+};
+
+class Employee
+{
+private:
+	std::string m_employer;
+	double m_wage{};
+
+public:
+	Employee(std::string_view employer, double wage)
+		: m_employer{ employer }, m_wage{ wage }
+	{}
+
+	const std::string& getEmployer() const { return m_employer; }
+	double getWage() const { return m_wage; }
+};
+
+//Teacher publicly inherits Person and Employee
+class Teacher : public Person, public Employee
+{
+private:
+	int m_teachesGrade{};
+
+public:
+	Teacher(std::string_view name, int age, 
+			std::string_view employer,
+			double wage, int teachesGrade)
+		: Person{ name, age }, Employee{ employer,wage },
+		m_teachesGrade{ teachesGrade }
+	{}
+
+	void printInfo()
+	{
+		std::cout << "Name: " << Person::getName() << '\n';
+		std::cout << "Age: " << Person::getAge() << '\n';
+		std::cout << "Employer: " << Employee::getEmployer() << '\n';
+		std::cout << "Wage: " << Employee::getWage() << '\n';
+		std::cout << "Grade to teach: " << m_teachesGrade << '\n';
+	}
+};
+
+int main()
+{
+	Teacher t{ "Mary", 45, "Boo", 14.3, 8 };
+
+	t.printInfo();
+
+	return 0;
+}
+//
